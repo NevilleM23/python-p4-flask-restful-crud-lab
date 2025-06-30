c25e8fc5d8eb49b08cbb6af6ae8fcc0c8fbdda67
Neville Muli <neville.muli@gmail.com>
2025-06-30 19:19:34 +0300

    finishes lab

--- server/app.py
+++ server/app.py
@@ -3,6 +3,7 @@
 from flask import Flask, jsonify, request, make_response
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
+from flask_cors import CORS
 
 from models import db, Plant
 
@@ -11,44 +12,74 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///plants.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
+CORS(app)
 migrate = Migrate(app, db)
-db.init_app(app)
 
-api = Api(app)
+db.init_app(app)  
 
+@app.route('/plants', methods=['GET'])
+def get_plants():
+    plants = Plant.query.all()
+    return jsonify([plant.to_dict() for plant in plants])
 
-class Plants(Resource):
+@app.route('/plants/<int:id>', methods=['GET'])
+def get_plant(id):
+    plant = Plant.query.get(id)
+    if plant:
+        return jsonify(plant.to_dict())
+    return make_response(jsonify({"error": "Plant not found"}), 404)
 
-    def get(self):
-        plants = [plant.to_dict() for plant in Plant.query.all()]
-        return make_response(jsonify(plants), 200)
-
-    def post(self):
-        data = request.get_json()
-
-        new_plant = Plant(
-            name=data['name'],
-            image=data['image'],
-            price=data['price'],
-        )
-
-        db.session.add(new_plant)
-        db.session.commit()
-
-        return make_response(new_plant.to_dict(), 201)
-
-
-api.add_resource(Plants, '/plants')
-
-
-class PlantByID(Resource):
-
-    def get(self, id):
-        plant = Plant.query.filter_by(id=id).first().to_dict()
-        return make_response(jsonify(plant), 200)
+@app.route('/plants', methods=['POST'])
+def create_plant():
+    data = request.get_json()
+    new_plant = Plant(
+        name=data['name'],
+        image=data['image'],
+        price=data['price'],
+        is_in_stock=data.get('is_in_stock', True)  
+    )
+    db.session.add(new_plant)
+    db.session.commit()
+    return jsonify(new_plant.to_dict()), 201
 
 
-api.add_resource(PlantByID, '/plants/<int:id>')
+@app.route('/plants/<int:id>', methods=['PATCH'])
+def update_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return make_response(jsonify({"error": "Plant not found"}), 404)
+    
+    data = request.get_json()
+    
+    if 'is_in_stock' in data:
+        plant.is_in_stock = data['is_in_stock']
+    # Commit changes
+    db.session.commit()
+  
+    return jsonify({
+        "id": plant.id,
+        "name": plant.name,
+        "image": plant.image,
+        "price": float(plant.price),  
+        "is_in_stock": plant.is_in_stock
+    })
+
+
+@app.route('/plants/<int:id>', methods=['DELETE'])
+def delete_plant(id):
+    plant = Plant.query.get(id)
+    if not plant:
+        return make_response(jsonify({"error": "Plant not found"}), 404)
+    
+    db.session.delete(plant)
+    db.session.commit()
+    
+    
+    return '', 204
+
+
+
+
 
 
 if __name__ == '__main__':
--- server/models.py
+++ server/models.py
@@ -1,16 +1,27 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
 
 class Plant(db.Model, SerializerMixin):
     __tablename__ = 'plants'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    image = db.Column(db.String)
-    price = db.Column(db.Float)
-    is_in_stock = db.Column(db.Boolean)
+    name = db.Column(db.String(100), nullable=False)
+    image = db.Column(db.String(255), nullable=False)
+    price = db.Column(db.Numeric(10, 2), nullable=False)  # For currency values
+    is_in_stock = db.Column(db.Boolean, default=True)
 
-    def __repr__(self):
-        return f'<Plant {self.name} | In Stock: {self.is_in_stock}>'
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "image": self.image,
+            "price": float(self.price),  # Convert Decimal to float
+            "is_in_stock": self.is_in_stock
+        }
